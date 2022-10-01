8dbcd8db6d3e9cb7b42fe13fee8bcb3c8a25eedb
Mohamed Refaat <medom193@gmail.com>
2022-10-01 17:59:45 +0200

    Add project files.

---
+++ TangyWeb_Server/wwwroot/JS/common.js
@@ -0,0 +1,36 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success")
+    {
+        toastr.success(message, "Toastr JS Success", { timeOut: 5000 });
+    }
+    if (type == "error")
+    {
+        toastr.error(message, "Toastr JS Failure", { timeOut: 5000 });
+    }
+}
+
+window.ShowSweet = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Successful Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
+
